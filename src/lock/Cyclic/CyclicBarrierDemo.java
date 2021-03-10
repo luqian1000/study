246c43a94e3745c08e9870cc9fd41f57632525db
luqian1000 <813991568@qq.com>
2021-03-10 15:25:45 +0800

    提交

--- src/lock/Cyclic/CyclicBarrierDemo.java
+++ src/lock/Cyclic/CyclicBarrierDemo.java
@@ -4,6 +4,7 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class CyclicBarrierDemo {
+    //测试
     public static void main(String[] args) {
         CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
             System.out.println(" 召唤神龙 ");
